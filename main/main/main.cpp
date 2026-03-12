3ad6f298169909942bc0ef52535483c2663c552f
iglapchev24 <IGLapchev24@codingburgas.bg>
2026-03-12 22:50:33 +0200

    Improve the main page design

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,13 +1,33 @@
 #include "raylib.h"
 int main() {
-    InitWindow(800, 600, "My Raylib App");
+    InitWindow(1400, 800, "EduCore Education");
     SetTargetFPS(60); 
+	Color eduBlue = { 0, 160, 233, 255 };   // RGB values for EduCore Blue
+	Color eduOrange = { 252, 176, 33, 255 }; // RGB values for EduCore Orange
+    Color mainBackground = { 10, 14, 20, 255 };
+	Color subSectionsBg = { 20, 30, 45, 255 }; // RGB values for background color for the main parts of the app
+	float spacing = 20.0f; // Spacing between elements
+	float separationLineY = 220.0f; // Y position for the separation line
+	float mainSectionHeight = GetScreenHeight() - separationLineY - spacing * 2; // Height for the main section
+	float mainSectionY = separationLineY + spacing; // Y position for the main section
+
+    Image image = LoadImage("eduCoreLogo.png");
+    ImageResize(&image, 200, 200);
+    Texture2D texture = LoadTextureFromImage(image);
+    UnloadImage(image);
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello, Raylib!", 300, 270, 30, DARKGRAY);
+        ClearBackground(mainBackground);
+		DrawText("Welcome to ", 20, 60, 70, eduBlue);
+        DrawText("EduCore Online School !", 20, 130, 70, eduOrange);
+        DrawTexture(texture, 1150, 10, WHITE);
+        DrawRectangle(40, separationLineY, GetScreenWidth() - 80, 2, eduBlue);
+        DrawRectangleRounded({ 40, separationLineY + 30, 1320 , mainSectionHeight }, 0.1f, 10, subSectionsBg);
+		DrawText(" Please choose subject", 550 , mainSectionY + 30, 30, RAYWHITE);
+		
         EndDrawing();
-    }     
+    }   
+    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
